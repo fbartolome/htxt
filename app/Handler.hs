f823a4ac5f6897239f8a8f54a5dfa80c0fa5f87a
Francisco Bartolomé <fbartolome18@gmail.com>
2018-02-21 17:14:34 -0300

    [WIP] Unlimited undo & redo

--- app/Handler.hs
+++ app/Handler.hs
@@ -12,23 +12,25 @@ import           State
 import           Style
 
 eventHandler :: State -> BrickEvent UIResource UIEvent -> EventM UIResource (Next State)
-eventHandler s (VtyEvent (V.EvKey V.KBS []))            = continue $ modifyText s delete
-eventHandler s (VtyEvent (V.EvKey V.KEnter []))         = continue $ modifyText s insertLine
-eventHandler s (VtyEvent (V.EvKey (V.KChar c) []))      = continue $ modifyText s (handleChar c)
-eventHandler s (VtyEvent (V.EvKey V.KUp []))            = continue $ modifyText s moveUp
-eventHandler s (VtyEvent (V.EvKey V.KDown []))          = continue $ modifyText s moveDown
-eventHandler s (VtyEvent (V.EvKey V.KLeft []))          = continue $ modifyText s moveLeft
-eventHandler s (VtyEvent (V.EvKey V.KLeft [V.MShift]))  = continue $ modifyText s selectLeft
-eventHandler s (VtyEvent (V.EvKey V.KRight []))         = continue $ modifyText s moveRight
-eventHandler s (VtyEvent (V.EvKey V.KRight [V.MShift])) = continue $ modifyText s selectRight
+eventHandler s (VtyEvent (V.EvKey V.KBS []))            = continue $ modifyText delete $ handleUndo s
+eventHandler s (VtyEvent (V.EvKey V.KEnter []))         = continue $ modifyText insertLine $ handleUndo s
+eventHandler s (VtyEvent (V.EvKey (V.KChar c) []))      = continue $ modifyText (handleChar c) $ handleUndo s
+eventHandler s (VtyEvent (V.EvKey V.KUp []))            = continue $ modifyText moveUp s
+eventHandler s (VtyEvent (V.EvKey V.KDown []))          = continue $ modifyText moveDown s
+eventHandler s (VtyEvent (V.EvKey V.KLeft []))          = continue $ modifyText moveLeft s
+eventHandler s (VtyEvent (V.EvKey V.KLeft [V.MShift]))  = continue $ modifyText selectLeft s
+eventHandler s (VtyEvent (V.EvKey V.KRight []))         = continue $ modifyText moveRight s
+eventHandler s (VtyEvent (V.EvKey V.KRight [V.MShift])) = continue $ modifyText selectRight s
+eventHandler s (VtyEvent (V.EvKey (V.KChar 'z') [V.MCtrl])) = continue $ undo s
+eventHandler s (VtyEvent (V.EvKey (V.KChar 'x') [V.MCtrl])) = continue $ redo s
 eventHandler s (VtyEvent (V.EvKey V.KEsc []))           = halt s
 eventHandler s (VtyEvent (V.EvResize rows cols))        = continue $ resize s rows cols
 eventHandler s (VtyEvent (V.EvKey V.KDown [V.MShift]))  = vScrollBy (viewportScroll EditorViewpoint) 1 >> continue s
 eventHandler s (VtyEvent (V.EvKey V.KUp   [V.MShift]))  = vScrollBy (viewportScroll EditorViewpoint) (-1) >> continue s
 eventHandler s _                                        = continue s
 
-modifyText :: State -> (Cursor StyleChar -> Cursor StyleChar) -> State
-modifyText s f = s {text = mapUnselected (\sc -> sc {style = Nothing})
+modifyText :: (Cursor StyleChar -> Cursor StyleChar) -> State -> State
+modifyText f s = s {text = mapUnselected (\sc -> sc {style = Nothing})
                            $ mapSelected (\sc -> sc {style = Just tiltOn})
                            $ f $ text s}
 
@@ -37,3 +39,8 @@ handleChar ch c = insert (StyleChar ch Nothing) c
 
 resize :: State -> Int -> Int -> State
 resize s rows cols = s {terminalSize = (rows, cols)}
+
+handleUndo :: State -> State
+handleUndo s = s {undoText = t:us, redoText = []} where
+  t = text s
+  us = undoText s
--- app/State.hs
+++ app/State.hs
@@ -3,6 +3,8 @@ module State
   , StyleChar (..)
   , State.empty
   , getFilename
+  , undo
+  , redo
   ) where
 
 import           Brick
@@ -16,14 +18,25 @@ data StyleChar = StyleChar
 
 data State = State
   { text         :: Cursor StyleChar
+  , undoText     :: [Cursor StyleChar]
+  , redoText     :: [Cursor StyleChar]
   , filename     :: Maybe String
   , terminalSize :: (Int, Int)
   }
 
 -- TODO en terminalSize buscar el tamaño de la terminal
 empty :: State
-empty = State {text = Cursor.empty, filename = Nothing, terminalSize = (30, 30)}
+empty = State {text = Cursor.empty, filename = Nothing, terminalSize = (30, 30),
+ undoText = [], redoText = []}
 
 getFilename :: State -> String
-getFilename (State _ Nothing _)  = "*Unsaved file*"
-getFilename (State _ (Just n) _) = n
+getFilename (State {filename = Nothing})  = "*Unsaved file*"
+getFilename (State {filename = (Just n)}) = n
+
+undo :: State -> State
+undo (State t (u:us) rs fn s) = State u us (t:rs) fn s
+undo s                        = s
+
+redo :: State -> State
+redo (State t us (r:rs) fn s) = State r (t:us) rs fn s
+redo s                        = s
